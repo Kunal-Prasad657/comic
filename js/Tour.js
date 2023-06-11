3885821e9a2876a2b0f813f633c72ad878861237
Kunal-Prasad657 <74914986+Kunal-Prasad657@users.noreply.github.com>
2023-06-12 02:38:25 +0530

    Add files via upload

---
+++ js/Tour.js
@@ -0,0 +1,96 @@
+AFRAME.registerComponent("tour", {
+  init: function () {
+    this.placesContainer = this.el;
+    this.createCards();
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "1",
+
+        url: "./assets/thumbnails/Bleach_cover_01.jpg",
+      },
+      {
+        id: "2",
+
+        url: "./assets/thumbnails/DB_Tankōbon.png",
+      },
+
+      {
+        id: "3",
+        url: "./assets/thumbnails/n.jpg",
+      },
+      {
+        id: "4",
+        url: "./assets/thumbnails/One_Piece,_Volume_61_Cover_(Japanese).jpg",
+      },
+    ];
+
+    let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position, item.id);
+
+      // Thumbnail Element
+      const thumbNail = this.createThumbNail(item);
+      borderEl.appendChild(thumbNail);
+
+      // Title Text Element
+      const titleEl = this.createTitleEl(position, item);
+      borderEl.appendChild(titleEl);
+
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function (position, id) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      height: 28,
+      width: 20,
+    });
+    entityEl.setAttribute("position", position);
+    entityEl.setAttribute("material", {
+      color: "#0077CC",
+      opacity: 1,
+    });
+
+    return entityEl;
+  },
+  createThumbNail: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      scale: 10,
+    });
+    entityEl.setAttribute("material", { src: item.url });
+
+    return entityEl;
+  },
+  createTitleEl: function (position, item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("text", {
+      font: "exo2bold",
+      align: "center",
+      width: 70,
+      color: "#e65100",
+      value: item.title,
+    });
+    const elPosition = position;
+    elPosition.y = -20;
+    entityEl.setAttribute("position", elPosition);
+    entityEl.setAttribute("visible", true);
+    return entityEl;
+  },
+});
